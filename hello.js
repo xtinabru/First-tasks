f04a52bf643deda09cc237884f0fb0dfb2689d47
Xtina Bru <141496377+xtinabru@users.noreply.github.com>
2023-09-12 14:36:12 +0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,18 @@
+// let courseName = "Intro to Programming";
+// let amountOfCredits = 5;
+// let startDate ="2020-09-01";
+//console.log("You are studying " + courseName + "." +" Course has started on " + startDate + " and it is worth "  + amountOfCredits+  " credits.");
+ process.stdout.write("Hello console!\n")
+
+let x = 5, y = 10, z = 15, sum;
+sum = x + y; //15
+y - x; //5
+x * y; //50
+y / x; //2
+x % 2; //1
+x++; //6
+x--; //4
+console.log("X is " + x + ", Y is " + y + ", Z is " + z +". Sum of X and Y is " + sum +'.');
+
+process.stdout.write ("Hello \nworld")
+
