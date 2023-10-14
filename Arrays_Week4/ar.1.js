197c5efc26ac4f76cd998624311c5f41f29966b4
Cristina Bruscagin <141496377+xtinabru@users.noreply.github.com>
2023-10-14 20:47:01 +0300

    Delete Arrays_Week4 directory

--- Arrays_Week4/ar.1.js
+++
@@ -1,9 +0,0 @@
-let fruitarray = ["banana", "apple", "kiwi", "orange", "blueberries"];
-
-
-fruitarray.push("pomogranate");
-process.stdout.write("\nItem added\n");
-
-for (let i = 0; i < fruitarray.length; i++) {
-  process.stdout.write(fruitarray[i] + '\n');
-}
